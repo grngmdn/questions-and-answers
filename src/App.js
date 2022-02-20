5f8c58236fc1e2811703f189be566d2dbd54810d
Madan gurung <gurungmadan@hotmail.com>
2022-02-20 07:48:22 +0000

    adding styles using mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from "react"
+import questions from "./Data"
+import Question from "./Question"
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+
+
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: 'center',
+        height: "100vh",
+        width: "100vw" 
+      }}
+    >
+        <Box
+          sx={{
+            backgroundColor: "#fefefe",
+            display:"flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: 'center', 
+            width: "700px",
+            paddingTop: "50px",
+            paddingBottom: "50px"
+          }}
+        >
+          <Typography
+              variant="h3"
+              sx={{
+                paddingBottom: "30px"
+              }}
+          >Q&A section
+          </Typography>
+          {questions.map(
+            question => <Question key={question.id} {...question} />
+          )}
+        </Box>
+    </Box>
   );
 }
 
---
+++ src/Question.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+
+
+
+function Question( {id, title, info} ) {
+
+    const [showInfo, setShowInfo] = useState(false)
+
+    const toggleInfo = () => {
+        setShowInfo(!showInfo)
+    }
+
+  return (
+    <Box>
+        <Card
+            sx={{ 
+                width: "600px",
+                // height: "120px",
+                padding: "20px" 
+            }}
+            elevation={24}
+        >
+        <Box
+            sx={{
+                display: "flex",
+                alignItems: 'center', 
+                alignContent: 'space-around',
+                justifyContent: 'space-between' 
+            }}
+        >
+            <Typography
+                variant="subtitle2"
+            >{title}
+            </Typography>
+            <Button
+                onClick={toggleInfo}
+            >
+                {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+            </Button>
+        </Box>
+        <Typography
+            variant="subtitle1"
+            color="textSecondary"
+        >{showInfo ? info : null}
+        </Typography>
+        </Card><br />
+    </Box>
+  )
+}
+
+export default Question
